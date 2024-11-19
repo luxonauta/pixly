b6233f1911b043344b14c482e0737758ac057908
Lucas de França <luxon4uta@gmail.com>
2024-11-19 04:39:28 -0300

    🍇 Remove background color from `Grid`

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -16,7 +16,7 @@ export const Grid: React.FC<EditorGridProps> = ({
   return (
     <div className="aspect-square w-full">
       <div
-        className="grid h-full w-full gap-px overflow-hidden rounded-lg border border-black/10 bg-[#DFDFDA]"
+        className="grid h-full w-full gap-px overflow-hidden rounded-lg border border-black/10"
         style={{
           gridTemplateColumns: `repeat(${gridSize}, minmax(0, 1fr))`
         }}
